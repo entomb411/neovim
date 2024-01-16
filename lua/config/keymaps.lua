70900be175ce2d6b91483772fde8d41fd2555ea3
Thomas Baillie <thomas.baillie@soundunited.com>
2024-01-16 14:26:47 +1100

    Add telescope-live-grep-args

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open telescope file_browser picker
+-- vim.api.nvim_set_keymap("n", "<space>fp", ":Telescope file_browser<CR>", {
+--   noremap = true,
+--   desc = "Open file browser",
+-- })
+
+-- Open telescope live_grep_args
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>fg",
+  ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+  { desc = "Live Grep (Args)" }
+)
