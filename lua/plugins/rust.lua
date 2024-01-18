e8d3a0230e47fbefebc48182b80f86fc0e0bd05d
Thomas Baillie <thomas.baillie@soundunited.com>
2024-01-18 14:03:54 +1100

    Wipe out everything so my fixed config can be the new master.

--- lua/plugins/rust.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- disable lspconfig for rust_analyzer
-  { "neovim/nvim-lspconfig", ft = { "rust" }, config = function() end },
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^3", -- Recommended
-    ft = { "rust" },
-  },
-}
