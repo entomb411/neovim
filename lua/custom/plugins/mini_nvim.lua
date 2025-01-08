3f8c3559a347fd6941586ca68a0dfb31f67383fd
Thomas Baillie <thomas.baillie@soundunited.com>
2025-01-08 12:34:08 +1100

    Use mini.pick in the start screen instead of telescope
    Also remove mini.statusline, since I'm using lualine instead

--- lua/custom/plugins/mini_nvim.lua
+++ lua/custom/plugins/mini_nvim.lua
@@ -126,31 +126,12 @@ return {
     -- Fast and flexible start screen
     local starter = require 'mini.starter'
     starter.setup {
-      dependencies = {
-        'telescope_nvim.lua',
-        'nvim-telescope/telescope-file-browser.nvim',
-      },
       items = {
-        starter.sections.telescope(),
+        starter.sections.pick(),
         starter.sections.sessions(5, true),
         starter.sections.builtin_actions(),
       },
     }
-
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
-    local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
-    statusline.setup { use_icons = vim.g.have_nerd_font }
-
-    -- You can configure sections in the statusline by overriding their
-    -- default behavior. For example, here we set the section for
-    -- cursor location to LINE:COLUMN
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
 
     -- Navigate and manipulate file system
     -- Run with ':lua MiniFiles.open()'
