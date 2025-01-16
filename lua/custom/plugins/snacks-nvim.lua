862f0121e498644df269fcd1769e923f4acc513a
Thomas B <tomb411.w@gmail.com>
2025-01-17 01:22:47 +1100

    Remove telescope and replace it with Snacks.picker
    Not everything can be replaced easily though

--- lua/custom/plugins/snacks-nvim.lua
+++ lua/custom/plugins/snacks-nvim.lua
@@ -1,78 +1,284 @@
 return {
-  "folke/snacks.nvim",
+  'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
+  enabled = true,
+  ---@type snacks.Config
   opts = {
-  --   bigfile = { enabled = true },
-  --   notifier = {
-  --     enabled = true,
-  --     timeout = 3000,
-  --   },
-  --   quickfile = { enabled = true },
-  --   statuscolumn = { enabled = true },
-  --   words = { enabled = true },
-  --   styles = {
-  --     notification = {
-  --       wo = { wrap = true } -- Wrap notifications
-  --     }
-  --   }
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = false },
+    dashboard = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = false },
+    notifier = { enabled = false },
+    quickfile = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    words = { enabled = false },
+    picker = {},
   },
-  -- keys = {
-  --   { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-  --   { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-  --   { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-  --   { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
-  --   { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
-  --   { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
-  --   { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
-  --   { "<leader>cR", function() Snacks.rename() end, desc = "Rename File" },
-  --   { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
-  --   { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
-  --   { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference" },
-  --   { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference" },
-  --   {
-  --     "<leader>N",
-  --     desc = "Neovim News",
-  --     function()
-  --       Snacks.win({
-  --         file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-  --         width = 0.6,
-  --         height = 0.6,
-  --         wo = {
-  --           spell = false,
-  --           wrap = false,
-  --           signcolumn = "yes",
-  --           statuscolumn = " ",
-  --           conceallevel = 3,
-  --         },
-  --       })
-  --     end,
-  --   }
-  -- },
-  -- init = function()
-  --   vim.api.nvim_create_autocmd("User", {
-  --     pattern = "VeryLazy",
-  --     callback = function()
-  --       -- Setup some globals for debugging (lazy-loaded)
-  --       _G.dd = function(...)
-  --         Snacks.debug.inspect(...)
-  --       end
-  --       _G.bt = function()
-  --         Snacks.debug.backtrace()
-  --       end
-  --       vim.print = _G.dd -- Override print to use snacks for `:=` command
-  --
-  --       -- Create some toggle mappings
-  --       Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-  --       Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-  --       Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-  --       Snacks.toggle.diagnostics():map("<leader>ud")
-  --       Snacks.toggle.line_number():map("<leader>ul")
-  --       Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
-  --       Snacks.toggle.treesitter():map("<leader>uT")
-  --       Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-  --       Snacks.toggle.inlay_hints():map("<leader>uh")
-  --     end,
-  --   })
-  -- end,
+  keys = {
+    -- Picker keybindings
+    {
+      '<leader>sx',
+      function()
+        Snacks.picker.pickers()
+      end,
+      desc = '[S]earch Built-In Pickers',
+    },
+    -- find
+    {
+      '<leader>sb',
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = '[S]earch [B]uffers',
+    },
+    {
+      '<leader>sn',
+      function()
+        Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = '[S]earch [N]eovim Config',
+    },
+    {
+      '<leader>sf',
+      function()
+        Snacks.picker.files()
+      end,
+      desc = '[S]earch [F]iles',
+    },
+    {
+      '<leader>s.',
+      function()
+        Snacks.picker.recent()
+      end,
+      desc = '[S]earch Recent Files',
+    },
+    -- git
+    {
+      '<leader>sGl',
+      function()
+        Snacks.picker.git_log()
+      end,
+      desc = '[S]earch [G]it [L]og',
+    },
+    {
+      '<leader>sGf',
+      function()
+        Snacks.picker.git_files()
+      end,
+      desc = '[S]earch [G]it [F]iles',
+    },
+    {
+      '<leader>sGs',
+      function()
+        Snacks.picker.git_status()
+      end,
+      desc = '[S]earch [G]it [S]tatus',
+    },
+    -- vim.keymap.set('n', '<leader>cgc', builtin.git_commits, { desc = 'Search [G]it [C]ommits' })
+    -- vim.keymap.set('n', '<leader>cgb', builtin.git_branches, { desc = 'Search [G]it [B]ranches' })
+    -- vim.keymap.set('n', '<leader>cgf', builtin.git_bcommits, { desc = 'Search [G]it Bu[f]fer Commits' })
+    -- Grep
+    {
+      '<leader>sL',
+      function()
+        Snacks.picker.lines()
+      end,
+      desc = '[S]earch Buffer [L]ines',
+    },
+    {
+      '<leader>sB',
+      function()
+        Snacks.picker.grep_buffers()
+      end,
+      desc = '[S]earch Open [B]uffers',
+    },
+    {
+      '<leader>sg',
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = '[S]earch by [G]rep',
+    },
+    {
+      '<leader>si',
+      function()
+        Snacks.picker.grep { ignored = true, format = 'file', finder = 'grep', live = true, supports_live = true }
+      end,
+      desc = '[S]earch by Grep No-[I]gnore',
+    },
+    {
+      '<leader>s/',
+      function()
+        Snacks.picker.grep { ignored = false, buffers = true, format = 'file', finder = 'grep', live = true, supports_live = true }
+      end,
+      desc = '[S]earch [/] by Grep in Open Files',
+    },
+    {
+      '<leader>sw',
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = '[S]earch [W]ord',
+      mode = { 'n', 'x' },
+    },
+    -- search
+    {
+      '<leader>s"',
+      function()
+        Snacks.picker.registers()
+      end,
+      desc = '[S]earch [R]egisters',
+    },
+    {
+      '<leader>sa',
+      function()
+        Snacks.picker.autocmds()
+      end,
+      desc = '[S]earch [A]utocmds',
+    },
+    {
+      '<leader>sH',
+      function()
+        Snacks.picker.command_history()
+      end,
+      desc = '[S]earch Command [H]istory',
+    },
+    {
+      '<leader>sC',
+      function()
+        Snacks.picker.commands()
+      end,
+      desc = '[S]earch [C]ommands',
+    },
+    {
+      '<leader>sd',
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = '[S]earch [D]iagnostics',
+    },
+    {
+      '<leader>sh',
+      function()
+        Snacks.picker.help()
+      end,
+      desc = '[S]earch [H]elp',
+    },
+    {
+      '<leader>sH',
+      function()
+        Snacks.picker.highlights()
+      end,
+      desc = '[S]earch [H]ighlights',
+    },
+    {
+      '<leader>sj',
+      function()
+        Snacks.picker.jumps()
+      end,
+      desc = '[S]earch [J]umplist',
+    },
+    {
+      '<leader>sk',
+      function()
+        Snacks.picker.keymaps()
+      end,
+      desc = '[S]earch [K]eymaps',
+    },
+    -- { '<leader>st', function() end, { desc = '[S]earch [T]ree-sitter' })
+    {
+      '<leader>sl',
+      function()
+        Snacks.picker.loclist()
+      end,
+      desc = 'Location List',
+    },
+    {
+      '<leader>sM',
+      function()
+        Snacks.picker.man()
+      end,
+      desc = '[S]earch [M]an Pages',
+    },
+    {
+      '<leader>sm',
+      function()
+        Snacks.picker.marks()
+      end,
+      desc = '[S]earch [M]arks',
+    },
+    {
+      '<leader>sr',
+      function()
+        Snacks.picker.resume()
+      end,
+      desc = '[S]earch [R]esume',
+    },
+    {
+      '<leader>sq',
+      function()
+        Snacks.picker.qflist()
+      end,
+      desc = '[S]earch [Q]uickfix List',
+    },
+    {
+      '<leader>sc',
+      function()
+        Snacks.picker.colorschemes()
+      end,
+      desc = '[S]earch [C]olorschemes',
+    },
+    {
+      '<leader>sp',
+      function()
+        Snacks.picker.projects()
+      end,
+      desc = '[S]earch [P]rojects',
+    },
+    -- LSP
+    {
+      'gd',
+      function()
+        Snacks.picker.lsp_definitions()
+      end,
+      desc = '[G]oto [D]efinition',
+    },
+    {
+      'gr',
+      function()
+        Snacks.picker.lsp_references()
+      end,
+      nowait = true,
+      desc = '[G]oto [R]eferences',
+    },
+    {
+      'gI',
+      function()
+        Snacks.picker.lsp_implementations()
+      end,
+      desc = '[G]oto [I]mplementation',
+    },
+    {
+      -- Jump to the type of the word under your cursor.
+      --  Useful when you're not sure what type a variable is and you want to see
+      --  the definition of its *type*, not where it was *defined*.
+      '<leader>D',
+      function()
+        Snacks.picker.lsp_type_definitions()
+      end,
+      desc = 'Type [D]efinition',
+    },
+    {
+      '<leader>sls',
+      function()
+        Snacks.picker.lsp_symbols()
+      end,
+      desc = '[S]earch [L]sp [S]ymbols',
+    },
+  },
 }
