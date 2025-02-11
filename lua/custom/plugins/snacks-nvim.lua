875b3cfafe6b61b78a13e9780af75d7909710d10
Thomas Baillie <thomas.baillie@soundunited.com>
2025-02-12 10:22:23 +1100

    Move git pane into separate function
    I want to assemble the dashboard opts dynamically. Actually opts can be
    a function returning a table, so I should do it that way.

--- lua/custom/plugins/snacks-nvim.lua
+++ lua/custom/plugins/snacks-nvim.lua
@@ -20,6 +20,30 @@ local get_cmd = function()
   end
 end
 
+local get_dashboard_git_section = function()
+        return {
+          pane = 2,
+          icon = ' ',
+          title = 'Git Status',
+          section = 'terminal',
+          -- enabled = false,
+          enabled = function()
+            -- Check if there is a .git folder in the root of the project.
+            -- TODO: Currently this only checks if it exists in the current directory. Should I check upwards?
+            return vim.fn.isdirectory '.git' == 1
+          end,
+          -- cmd = 'git status --short --branch --renames',
+          cmd = 'krabby random ; sleep .1',
+          random = 10,
+          height = 30,
+
+          -- height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        }
+end
+
 return {
   'folke/snacks.nvim',
   priority = 1000,
@@ -50,21 +74,27 @@ return {
         { section = 'keys', gap = 1, padding = 1 },
         { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
         { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
-        {
-          pane = 2,
-          icon = ' ',
-          title = 'Git Status',
-          section = 'terminal',
-          enabled = false,
-          -- enabled = function()
-          --   return Snacks.git.get_root() ~= nil
-          -- end,
-          cmd = 'git status --short --branch --renames',
-          height = 5,
-          padding = 1,
-          ttl = 5 * 60,
-          indent = 3,
-        },
+        -- {
+        --   pane = 2,
+        --   icon = ' ',
+        --   title = 'Git Status',
+        --   section = 'terminal',
+        --   -- enabled = false,
+        --   enabled = function()
+        --     -- Check if there is a .git folder in the root of the project.
+        --     -- TODO: Currently this only checks if it exists in the current directory. Should I check upwards?
+        --     return vim.fn.isdirectory '.git' == 1
+        --   end,
+        --   -- cmd = 'git status --short --branch --renames',
+        --   cmd = 'krabby random ; sleep .1',
+        --   random = 10,
+        --   height = 30,
+        --
+        --   -- height = 5,
+        --   padding = 1,
+        --   ttl = 5 * 60,
+        --   indent = 3,
+        -- },
         { section = 'startup' },
       },
     },
