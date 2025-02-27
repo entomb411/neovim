7b7093ed23a98fb2ec108a1dd5a5955be1fce207
Thomas Baillie <thomas.baillie@soundunited.com>
2025-02-27 17:26:09 +1100

    Use OSC52 clipboard
    This will allow, for example, copying test over SSH.

--- init.lua
+++ init.lua
@@ -117,6 +117,17 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
 
 -- Disable swapfile
 vim.opt.swapfile = false
